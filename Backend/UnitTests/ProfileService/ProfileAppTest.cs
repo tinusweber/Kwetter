613d7d7eb228d8232d191fbf0dbdf14cd47428de
Martijn <439924@student.fontys.nl>
2023-04-12 16:05:06 +0200

    change

---
+++ Backend/UnitTests/ProfileService/ProfileAppTest.cs
@@ -0,0 +1,92 @@
+﻿using ProfileService.Application;
+using ProfileService.Data.Models;
+using ProfileService.Data;
+using Moq;
+
+namespace profileService.Tests
+{
+    [TestFixture]
+    public class ProfileAppTest
+    {
+        private Mock<IProfileRepository> profileRepositoryMock;
+        private ProfileApp profileApp;
+
+        [SetUp]
+        public void Setup()
+        {
+            profileRepositoryMock = new Mock<IProfileRepository>();
+            profileApp = new ProfileApp(profileRepositoryMock.Object);
+        }
+
+        [Test]
+        public async Task FollowUser_ShouldCallRepository()
+        {
+            Guid userId = Guid.NewGuid();
+            Guid userToFollow = Guid.NewGuid();
+
+            await profileApp.FollowUser(userId, userToFollow);
+
+            profileRepositoryMock.Verify(x => x.FollowUser(userId, userToFollow), Times.Once);
+        }
+
+        [Test]
+        public void GetProfile_ShouldCallRepository()
+        {
+            Guid userId = Guid.NewGuid();
+
+            profileApp.GetProfile(userId);
+
+            profileRepositoryMock.Verify(x => x.GetProfile(userId), Times.Once);
+        }
+
+        [Test]
+        public async Task UnfollowUser_ShouldCallRepository()
+        {
+            Guid userId = Guid.NewGuid();
+            Guid userToUnfollow = Guid.NewGuid();
+
+            await profileApp.UnfollowUser(userId, userToUnfollow);
+
+            profileRepositoryMock.Verify(x => x.UnfollowUser(userId, userToUnfollow), Times.Once);
+        }
+
+        [Test]
+        public async Task UpdateProfile_ShouldCallRepository()
+        {
+            Guid userId = Guid.NewGuid();
+            Profile profileData = new Profile { Username = "John Doe", Biography = "hi" };
+
+            await profileApp.UpdateProfile(userId, profileData);
+
+            profileRepositoryMock.Verify(x => x.UpdateProfile(userId, profileData), Times.Once);
+        }
+
+        [Test]
+        public void GetAll_ShouldCallRepository()
+        {
+            List<Profile> expectedProfiles = new List<Profile>
+            {
+                new Profile { OwnerId = Guid.NewGuid(), Username = "John Doe", Biography = "hi" },
+                new Profile { OwnerId = Guid.NewGuid(), Username = "Jane Doe", Biography = "bye" }
+            };
+
+            profileRepositoryMock.Setup(x => x.GetAll()).ReturnsAsync(expectedProfiles);
+
+            Task<List<Profile>> actualProfilesTask = profileApp.GetAll();
+
+            profileRepositoryMock.Verify(x => x.GetAll(), Times.Once);
+
+            List<Profile> actualProfiles = actualProfilesTask.Result;
+            Assert.That(actualProfiles, Has.Count.EqualTo(expectedProfiles.Count));
+            Assert.Multiple(() =>
+            {
+                Assert.That(actualProfiles[0].OwnerId, Is.EqualTo(expectedProfiles[0].OwnerId));
+                Assert.That(actualProfiles[0].Username, Is.EqualTo(expectedProfiles[0].Username));
+                Assert.That(actualProfiles[0].Biography, Is.EqualTo(expectedProfiles[0].Biography));
+                Assert.That(actualProfiles[1].OwnerId, Is.EqualTo(expectedProfiles[1].OwnerId));
+                Assert.That(actualProfiles[1].Username, Is.EqualTo(expectedProfiles[1].Username));
+                Assert.That(actualProfiles[1].Biography, Is.EqualTo(expectedProfiles[1].Biography));
+            });
+        }
+    }
+}
---
+++ Backend/UnitTests/ProfileService/ProfileControllerTest.cs
@@ -0,0 +1,141 @@
+﻿using Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NSubstitute;
+using ProfileService.Api;
+using ProfileService.Api.Controllers;
+using ProfileService.Application;
+using ProfileService.Data;
+using ProfileService.Data.Models;
+using System.Security.Claims;
+
+namespace profileService.Tests
+{
+    [TestFixture]
+    public class ProfileControllerTest
+    {
+        private IProfileRepository mockRepository;
+        private ProfileApp mockProfileApp;
+        private ProfileController controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            mockRepository = Substitute.For<IProfileRepository>();
+            mockProfileApp = new ProfileApp(mockRepository);
+            controller = new ProfileController(mockProfileApp);
+        }
+/*
+        [Test]
+        public async Task GetAll_ReturnsListOfProfiles()
+        {
+            // Arrange
+            var expectedProfiles = new List<ProfileData>()
+            {
+                new ProfileData() { OwnerId = Guid.NewGuid(), Username = "johndoe", Biography = "Hello world", ProfilePictureBase64 = null },
+                new ProfileData() { OwnerId = Guid.NewGuid(), Username = "janedoe", Biography = "Lorem ipsum", ProfilePictureBase64 = null },
+            };
+
+            mockProfileApp.GetAll().Returns(Task.FromResult(expectedProfiles));
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            controller.HttpContext.Items["userId"] = Guid.NewGuid();
+
+            // Act
+            var result = await controller.GetAll();
+
+            // Assert
+            Assert.That(result, Is.InstanceOf<List<Profile>>());
+            Assert.That(result, Has.Count.EqualTo(expectedProfiles.Count));
+            Assert.Multiple(() =>
+            {
+                Assert.That(result[0].OwnerId, Is.EqualTo(expectedProfiles[0].OwnerId));
+                Assert.That(result[0].DisplayName, Is.EqualTo(expectedProfiles[0].Username));
+                Assert.That(result[0].Biography, Is.EqualTo(expectedProfiles[0].Biography));
+                Assert.That(result[0].ProfilePictureBase64, Is.EqualTo(expectedProfiles[0].ProfilePictureBase64));
+                Assert.That(result[1].OwnerId, Is.EqualTo(expectedProfiles[1].OwnerId));
+                Assert.That(result[1].DisplayName, Is.EqualTo(expectedProfiles[1].Username));
+                Assert.That(result[1].Biography, Is.EqualTo(expectedProfiles[1].Biography));
+                Assert.That(result[1].ProfilePictureBase64, Is.EqualTo(expectedProfiles[1].ProfilePictureBase64));
+            });
+        }
+
+        [Test]
+        public void Get_ReturnsUserProfile()
+        {
+            var expectedProfile = new ProfileData()
+            {
+                OwnerId = Guid.NewGuid(),
+                Username = "johndoe",
+                Biography = "I am John Doe",
+                ProfilePictureBase64 = "base64"
+            };
+            var profileController = new ProfileController(mockProfileApp);
+
+            var controllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = Mock.Of<ClaimsPrincipal>() }
+            };
+            controllerContext.HttpContext.Request.Headers["Authorization"] = "Bearer {token}";
+            profileController.ControllerContext = controllerContext;
+
+            mockProfileApp.GetProfile(Arg.Any<Guid>()).Returns(expectedProfile);
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.That(result, Is.InstanceOf<Profile>());
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.OwnerId, Is.EqualTo(expectedProfile.OwnerId));
+                Assert.That(result.DisplayName, Is.EqualTo(expectedProfile.Username));
+                Assert.That(result.Biography, Is.EqualTo(expectedProfile.Biography));
+                Assert.That(result.ProfilePictureBase64, Is.EqualTo(expectedProfile.ProfilePictureBase64));
+            });
+        }
+
+        [Test]
+        public void GetById_ReturnsProfileWithBlockStatus()
+        {
+            // Arrange
+            var profileId = Guid.NewGuid();
+            var expectedProfile = new ProfileData() { OwnerId = profileId, Username = "John Doe", Biography = "Hello world", ProfilePictureBase64 = null };
+
+            mockProfileApp.GetProfile(profileId).Returns(expectedProfile);
+            mockProfileApp.GetProfile(Arg.Any<Guid>()).Returns(new ProfileData() { BlockedUsers = new List<Guid>() { profileId } });
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            controller.HttpContext.Items["userId"] = Guid.NewGuid();
+
+            // Act
+            var result = controller.GetById(profileId.ToString());
+
+            // Assert
+            Assert.That(result, Is.InstanceOf<Profile>());
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.OwnerId, Is.EqualTo(expectedProfile.OwnerId));
+                Assert.That(result.DisplayName, Is.EqualTo(expectedProfile.Username));
+                Assert.That(result.Biography, Is.EqualTo(expectedProfile.Biography));
+                Assert.That(result.ProfilePictureBase64, Is.EqualTo(expectedProfile.ProfilePictureBase64));
+                Assert.That(result.Blocked, Is.True);
+            });
+        }
+*/
+        [Test]
+        public async Task UpdateProfile_ReturnsOk()
+        {
+            // Arrange
+            var profileId = Guid.NewGuid();
+            var profileUpdate = new ProfileUpdate() { DisplayName = "John Doe", Biography = "Hello world", ProfilePictureBase64 = null };
+            mockProfileApp.UpdateProfile(profileId, Arg.Any<ProfileService.Data.Models.Profile>()).Returns(Task.FromResult<ProfileService.Data.Models.Profile>(new ProfileService.Data.Models.Profile() { OwnerId = profileId }));
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+
+            // Act
+            var result = await controller.UpdateProfile(profileId.ToString(), profileUpdate);
+
+            // Assert
+            Assert.That(result, Is.InstanceOf<OkResult>());
+        }
+    }
+}
