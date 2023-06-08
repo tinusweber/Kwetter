b81f0d6d52fcebebfd4579abfee8ffd153efef95
Martijn <439924@student.fontys.nl>
2023-06-08 14:03:07 +0200

    changes

--- Backend/Gateway/Startup.cs
+++ Backend/Gateway/Startup.cs
@@ -1,15 +1,10 @@
 using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
-using Ocelot.Middleware;
 using Ocelot.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Ocelot.Middleware;
 
 namespace FS_Gateway
 {
@@ -22,26 +17,11 @@ namespace FS_Gateway
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(c =>
-                {
-                    c.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    c.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                })
-                .AddJwtBearer("TestKey", o =>
-                {
-                    o.RequireHttpsMetadata = false;
-                    o.Authority = Environment.GetEnvironmentVariable("AuthServer");
-                    o.Audience = "myresourceapi";
-                });
-
             services.AddOcelot();
-
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             await app.UseOcelot();
